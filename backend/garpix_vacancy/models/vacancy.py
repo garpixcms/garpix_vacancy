00580965568234d172a8b96ec08f56e0148a9d35
oem <azarov@garpix.com>
2021-08-12 11:26:10 +0300

    fix null was not available for fields

--- backend/garpix_vacancy/models/vacancy.py
+++ backend/garpix_vacancy/models/vacancy.py
@@ -8,14 +8,15 @@ VacancyMixin = import_string(settings.GARPIX_VACANCY_MIXIN)
 
 
 class Vacancy(VacancyMixin, models.Model):
-    position = models.CharField(max_length=300, verbose_name='Должность', blank=True)
-    city = models.CharField(max_length=300, verbose_name='Город', blank=True)
-    salary = models.CharField(max_length=300, verbose_name='Зарплата', blank=True, help_text='Пример: от 95 000 руб.')
-    short_description = models.CharField(max_length=1000, verbose_name='Краткое описание', blank=True)
-    requirements = RichTextUploadingField(verbose_name='Требования', blank=True)
-    conditions = RichTextUploadingField(verbose_name='Условия', blank=True)
-    full_name = models.CharField(max_length=100, verbose_name='ФИО интервьюера', blank=True)
-    description = RichTextUploadingField(blank=True, verbose_name='Описание')
+    position = models.CharField(max_length=300, verbose_name='Должность', blank=True, null=True)
+    city = models.CharField(max_length=300, verbose_name='Город', blank=True, null=True)
+    salary = models.CharField(max_length=300, verbose_name='Зарплата', blank=True, null=True,
+                              help_text='Пример: от 95 000 руб.')
+    short_description = models.CharField(max_length=1000, verbose_name='Краткое описание', blank=True, null=True)
+    requirements = RichTextUploadingField(verbose_name='Требования', blank=True, null=True)
+    conditions = RichTextUploadingField(verbose_name='Условия', blank=True, null=True)
+    full_name = models.CharField(max_length=100, verbose_name='ФИО интервьюера', blank=True, null=True)
+    description = RichTextUploadingField(blank=True, verbose_name='Описание', null=True)
     contacts = models.ForeignKey(Contact, on_delete=models.SET_NULL, verbose_name='Контакты', blank=True, null=True)
 
     def __str__(self):
